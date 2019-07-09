8098f6fa149574dccd716f13642a54820569e024
zhangchaojie <1098626505@qq.com>
2019-07-09 11:31:01 +0800

    更新readme

--- example/App.vue
+++ example/App.vue
@@ -27,8 +27,7 @@ export default {
           attrs: {
             action: 'https://jsonplaceholder.typicode.com/posts',
             responseFn (response, file) {
-              return 'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg'
-              // return file.url
+              return file.url
             }
           }
         }
